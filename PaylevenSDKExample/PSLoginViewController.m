10de2482ec8a648b169214c2186f4393ba4fdab5
Markus Kopf <kopf.markus@googlemail.com>
2015-10-29 09:54:23 +0100

    Version 1.1.0 (10-29-15)

--- PaylevenSDKExample/PSLoginViewController.m
+++ PaylevenSDKExample/PSLoginViewController.m
@@ -95,9 +95,7 @@
     //self.username.text = @"testeIT5@sharklasers.com";
     //self.username.text = @"mposuk@sharklasers.com"
     //self.username.text = @"mpospl@sharklasers.com";
-    //self.username.text = @"testeIT5@sharklasers.com";
-    
-    self.username.text = @"testlive16@sharklasers.com";
+    self.username.text = @"testeIT5@sharklasers.com";
     self.password.text = @"12345678";
 }
 
